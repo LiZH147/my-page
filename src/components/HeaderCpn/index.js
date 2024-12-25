cc15e932cf96f95923d04d8fd68af0e4896b2c4c
Zihan <1723893881@qq.com>
2024-12-25 20:58:45 +0800

    个人网站v0.1 完成Header控件

--- src/components/HeaderCpn/index.js
+++ src/components/HeaderCpn/index.js
@@ -4,7 +4,10 @@ import { Avatar } from "antd";
 import "./index.css"
 
 export default function HeaderCpn() {
-  const JikeAvatar = require('../../asset/JikeAvatar.png')
+  const JikeAvatar = require('../../asset/JikeAvatar.png');
+  const WeixinAvatar = require('../../asset/WeixinAvatar.js').WeixinAvatar;
+  const QQAvatar = require('../../asset/QQAvatar.png')
+
   return (
     <div className='headerWrapper'>
       <div className='webAvatar'>
@@ -13,9 +16,9 @@ export default function HeaderCpn() {
       </div>
       <div className='myLinks'>
         <div className='leftBox'>
-          <MyAvatar avatarSrc={JikeAvatar} link = {'https://okjk.co/ac6lkY'} />
-          <MyAvatar avatarSrc={JikeAvatar} />
-          <MyAvatar avatarSrc={JikeAvatar} />
+          <MyAvatar title={'即刻'} avatarSrc={JikeAvatar} link={'https://okjk.co/ac6lkY'} />
+          <MyAvatar title={'微信'} avatarSrc={WeixinAvatar} modalSrc={require('../../asset/WeixinCode.jpg')} />
+          <MyAvatar title={'QQ'} avatarSrc={QQAvatar} modalSrc={require('../../asset/QQCode.jpg')} />
         </div>
       </div>
     </div>
--- src/components/myAvatar/index.js
+++ src/components/myAvatar/index.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import { Avatar } from "antd";
+import React, { useState } from 'react'
+import { Avatar, Modal } from "antd";
 import './index.css'
 
 export default function MyAvatar(props) {
+
     const goToLink = () => {
         window.open(props.link, '_blank')
     }
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const handleCancel = (e) => {
+        e.stopPropagation()
+        setIsModalOpen(false);
+    };
+    const show2Code = () => {
+        setIsModalOpen(true);
+
+    }
+
     return (
-        <div className='main' onClick={goToLink} >
-            <Avatar size={48} src={props.avatarSrc} />
+        <div className='main' onClick={props.title === '即刻' ? goToLink : show2Code } >
+            <Avatar size={32} src={props.avatarSrc} />
+            {/* <span style={{marginLeft: '8px'}}>{props.title}</span> */}
+            <Modal title={props.title} open={isModalOpen} footer={null} onCancel={handleCancel}>
+                <img style={{width: '100%'}} src={props.modalSrc} />
+            </Modal>
         </div>
     )
 }
